8cd178c75da23300f6e873e70b552dde4ecfd145
tvkwoodlands <tvkwoodlands@gmail.com>
2014-02-11 22:24:06 -0600

    Eraser progress: actually works now

--- SmartStroke/TrailsTest.xaml.cs
+++ SmartStroke/TrailsTest.xaml.cs
@@ -27,13 +27,17 @@ namespace SmartStroke
     /// </summary>
     public sealed partial class TrailsTest : Page
     {
+        const double DRAW_WIDTH = 4.0;
+        const double ERASE_WIDTH = 30.0;
+        private Color DRAW_COLOR = Colors.Blue;
+        private Color ERASE_COLOR = Colors.White;
         //class member variables
         InkManager ink_manager = new Windows.UI.Input.Inking.InkManager();
         private uint pen_id;
         private uint touch_id;
         private Point previous_contact_pt;
         private Point current_contact_pt;
-        private Color current_color;
+        private bool erasing;
 
         DispatcherTimer timer;
 
@@ -47,7 +51,7 @@ namespace SmartStroke
             MyCanvas.PointerReleased += new PointerEventHandler(MyCanvas_PointerReleased);
             MyCanvas.PointerExited += new PointerEventHandler(MyCanvas_PointerReleased);
 
-            current_color = Colors.Blue;
+            erasing = false;
 
             //initialize timer
             timer = new DispatcherTimer();
@@ -116,21 +120,35 @@ namespace SmartStroke
 
                 if (Distance(x1, y1, x2, y2) > 2.0) //test whether the pointer has moved far enough to warrant drawing a new line
                 {
-                    Line line = new Line()
+                    Ellipse ellipse;
+                    if (erasing)
                     {
-                        X1 = x1,
-                        Y1 = y1,
-                        X2 = x2,
-                        Y2 = y2,
-                        StrokeThickness = 4.0,
-                        Stroke = new SolidColorBrush(current_color)
-                    };
-
+                        ellipse = new Ellipse()
+                        {
+                            Margin = new Thickness(x1, y1, x2, y2),
+                            Height = ERASE_WIDTH,
+                            Width = ERASE_WIDTH,
+                            Fill = new SolidColorBrush(ERASE_COLOR)
+                        };
+                        MyCanvas.Children.Add(ellipse);
+                    }
+                    else
+                    {
+                        Line line = new Line()
+                        {
+                            //Margin = new Thickness(x1, y1, x2, y2),
+                            X1 = x1, X2 = x2, Y1 = y1, Y2 = y2,
+                            //Height = DRAW_WIDTH,
+                            //Width = DRAW_WIDTH,
+                            StrokeThickness = DRAW_WIDTH,
+                            Stroke = new SolidColorBrush(DRAW_COLOR)
+                        };
+                        MyCanvas.Children.Add(line);
+                    }
+                    
                     previous_contact_pt = current_contact_pt;
 
-                    // Draw the line on the canvas by adding the Line object as
-                    // a child of the Canvas object.
-                    MyCanvas.Children.Add(line);
+                    //MyCanvas.Children.Add(ellipse);
 
                     // Pass the pointer information to the InkManager.
                     ink_manager.ProcessPointerUpdate(pt);
@@ -162,19 +180,20 @@ namespace SmartStroke
                 //first check if the stylus' eraser is being used
                 if (pt.Properties.IsEraser)
                 {
-                    current_color = Colors.White;
                     ink_manager.Mode = Windows.UI.Input.Inking.InkManipulationMode.Erasing;
+                    erasing = true;
                     //selCanvas.style.cursor = "url(images/erase.cur), auto"; 
                 }
                 else
                 {
-                    current_color = Colors.Blue;
-                    // Pass the pointer information to the InkManager.
-                    ink_manager.ProcessPointerDown(pt);
-                    pen_id = pt.PointerId;
-
-                    e.Handled = true;
+                    erasing = false;
                 }
+
+                // Pass the pointer information to the InkManager.
+                ink_manager.ProcessPointerDown(pt);
+                pen_id = pt.PointerId;
+
+                e.Handled = true;
             }
             else if (pointerDevType == PointerDeviceType.Touch)
             {
