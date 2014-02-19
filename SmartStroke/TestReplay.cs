ae78b279d2f653252e1a669a294e210658b3611b
imbiez <joshrispoli@gmail.com>
2014-02-19 15:22:26 -0600

    Updated TestReplay.cs

--- SmartStroke/TestReplay.cs
+++ SmartStroke/TestReplay.cs
@@ -3,38 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Windows.UI.Input.Inking;
 using System.Collections;
+using Windows.UI.Input.Inking;
+using Windows.UI.Xaml.Shapes;
 
 namespace SmartStroke
 {
+    public enum ACTION_TYPE { STROKE }
     public abstract class TestAction
     {
         protected DateTime startTime;
         protected DateTime endTime;
         public TestAction() { }
+        public TestAction(DateTime StartTime)
+        {
+            startTime = StartTime;
+        }
         public TestAction(DateTime StartTime, DateTime EndTime)
         {
             startTime = StartTime;
             endTime = EndTime;
         }
-        public abstract void executeAction();
+        public void startAction(DateTime StartTime) 
+        {
+            startTime = StartTime;
+        }
+        public void endAction(DateTime EndTime)
+        {
+            endTime = EndTime;
+        }
+        public abstract ACTION_TYPE getActionType();
     }
     public sealed class Stroke : TestAction
     {
-        InkStrokeRenderingSegment strokeData;
-        public Stroke(DateTime StartTime, DateTime EndTime,
-            InkStrokeRenderingSegment StrokeData)
-            : base(StartTime, EndTime)
-        {
-            strokeData = StrokeData;
+        Line line;
+        public Stroke(DateTime StartTime) : base(StartTime) { }
+        public override ACTION_TYPE getActionType() 
+        { 
+            return ACTION_TYPE.STROKE; 
         }
-        public override void executeAction() { }
-    }
-    public sealed class ClearScreen : TestAction
-    {
-        public ClearScreen(){}
-        public override void executeAction() { }
     }
     public sealed class TestReplay
     {
@@ -47,6 +54,20 @@ namespace SmartStroke
         }
         public void startTest() { startTime = DateTime.Now; }
         public void endTest() { endTime = DateTime.Now; }
-        public void addAction(TestAction action) { testActions.Add(action); }
+        public void startStroke(DateTime StartTime)
+        {
+            Stroke stroke = new Stroke(StartTime);
+            testActions.Add(stroke);
+        }
+        public void endStroke(DateTime EndTime)
+        {
+            for (int i = testActions.Count - 1; i >= 0; i++)
+            {
+                if (testActions[i].getActionType() == ACTION_TYPE.STROKE)
+                {
+                    testActions[i].endAction(EndTime);
+                }
+            }
+        }
     }
 }
