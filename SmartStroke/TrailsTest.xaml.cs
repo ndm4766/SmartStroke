2f1cfd123c4489e0d11c9adcb0d22c14359fb2b8
tvkwoodlands <tvkwoodlands@gmail.com>
2014-02-18 21:52:06 -0600

    I swear it builds now

--- SmartStroke/TrailsTest.xaml.cs
+++ SmartStroke/TrailsTest.xaml.cs
@@ -19,7 +19,7 @@ using Windows.Devices.Input;
 using Windows.UI.ApplicationSettings;
 using System.Diagnostics;
 using Windows.Graphics.Display;
-using System.Management;
+
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
