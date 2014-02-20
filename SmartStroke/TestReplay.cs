c9532b7686dc995f669eb6e4d0338520ec488cb6
imbiez <joshrispoli@gmail.com>
2014-02-19 22:01:21 -0600

    Updated TestReplay

--- SmartStroke/TestReplay.cs
+++ SmartStroke/TestReplay.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Collections;
+using System.Diagnostics;
 using Windows.UI.Input.Inking;
 using Windows.UI.Xaml.Shapes;
 
@@ -14,30 +15,60 @@ namespace SmartStroke
     {
         protected DateTime startTime;
         protected DateTime endTime;
-        public TestAction() { }
+        protected bool finished;
+        public TestAction() 
+        {
+            startTime = DateTime.Now;
+            finished = false; 
+        }
         public TestAction(DateTime StartTime)
         {
             startTime = StartTime;
+            finished = false;
         }
         public TestAction(DateTime StartTime, DateTime EndTime)
         {
             startTime = StartTime;
             endTime = EndTime;
+            finished = true;
         }
         public void startAction(DateTime StartTime) 
         {
             startTime = StartTime;
+            finished = false;
+        }
+        public void startAction()
+        {
+            startTime = DateTime.Now;
+            finished = false;
         }
         public void endAction(DateTime EndTime)
         {
             endTime = EndTime;
+            finished = true;
         }
+        public void endAction()
+        {
+            endTime = DateTime.Now;
+            finished = true;
+        }
+        public bool isFinished() { return finished; }
         public abstract ACTION_TYPE getActionType();
     }
     public sealed class Stroke : TestAction
     {
-        Line line;
-        public Stroke(DateTime StartTime) : base(StartTime) { }
+        private class LineData
+        {
+            private DateTime startTime;
+            private Line line;
+            public LineData(DateTime StartTime, Line _Line) 
+            {
+                startTime = StartTime;
+                line = _Line;
+            }
+        }
+        private List<LineData> lines;
+        public Stroke() { lines = new List<LineData>(); }
         public override ACTION_TYPE getActionType() 
         { 
             return ACTION_TYPE.STROKE; 
@@ -48,26 +79,48 @@ namespace SmartStroke
         private DateTime startTime;
         private DateTime endTime;
         private List<TestAction> testActions;
+        private Stroke currentStroke;
         public TestReplay()
         {
             testActions = new List<TestAction>();
         }
         public void startTest() { startTime = DateTime.Now; }
         public void endTest() { endTime = DateTime.Now; }
-        public void startStroke(DateTime StartTime)
+        public void beginStroke()
         {
-            Stroke stroke = new Stroke(StartTime);
-            testActions.Add(stroke);
-        }
-        public void endStroke(DateTime EndTime)
-        {
-            for (int i = testActions.Count - 1; i >= 0; i++)
+            if (getCurrentTestAction() != null)
             {
-                if (testActions[i].getActionType() == ACTION_TYPE.STROKE)
-                {
-                    testActions[i].endAction(EndTime);
-                }
+                Debug.Assert(getCurrentTestAction().isFinished()
+                    , "Current test action is unfinished.");
+                currentStroke = new Stroke();
+                testActions.Add(currentStroke);
             }
+        }
+        public void endStroke()
+        {
+            if (getCurrentTestAction() != null)
+            {
+                Debug.Assert(!getCurrentTestAction().isFinished()
+                    , "Current test action is already finished.");
+                Debug.Assert(checkCurrentTestAction(ACTION_TYPE.STROKE)
+                    , "Current test action is already ended."); 
+                currentStroke.endAction();
+                currentStroke = new Stroke();
+            }
+        }
+        public void addLine(Line line)
+        {
+            
+        }
+        public bool checkCurrentTestAction(ACTION_TYPE act)
+        {
+            if (getCurrentTestAction() == null) return false;
+            else return getCurrentTestAction().getActionType() == act;
+        }
+        public TestAction getCurrentTestAction()
+        {
+            if (testActions.Count == 0) return null;
+            else return testActions[testActions.Count - 1];
         }
     }
 }
