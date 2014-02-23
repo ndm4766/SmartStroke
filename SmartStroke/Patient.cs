4e6f3a35723c02aee5e9285254eb1147fec5df95
imbiez <joshrispoli@gmail.com>
2014-02-23 16:50:12 -0600

    Added patient notes to Patient class

--- SmartStroke/Patient.cs
+++ SmartStroke/Patient.cs
@@ -8,12 +8,29 @@ namespace SmartStroke
 {
     public enum GENDER { MALE, FEMALE }
     public enum EDU_LEVEL { HIGHSCHOOL, ASSOCIATES, BACHELORS, MASTERS, PHD }
+    class PatientNote
+    {
+        private string note;
+        private DateTime time;
+        public PatientNote(string Note) { note = Note; }
+        public PatientNote(string Note, DateTime Time)
+        {
+            note = Note;
+            time = Time;
+        }
+        public void changeNote(string Note) {
+            note = Note;
+        }
+        public string getNote() { return note; }
+        public DateTime getTime() { return time; }
+    }
     class Patient
     {
         private string name;
         private DateTime birthDate;
         private GENDER gender;
         private EDU_LEVEL eduLevel;
+        private List<PatientNote> notes;
         public Patient(string Name, DateTime BirthDate,
             GENDER Gender, EDU_LEVEL EduLevel)
         {
@@ -21,10 +38,12 @@ namespace SmartStroke
             birthDate = BirthDate;
             gender = Gender;
             eduLevel = EduLevel;
+            notes = new List<PatientNote>();
         }
         public string getName() { return name; }
         public DateTime getBirthDate() { return birthDate; }
         public GENDER getGender() { return gender; }
         public EDU_LEVEL getEduLevel() { return eduLevel; }
+        public List<PatientNote> getNotes() { return notes; }
     }
 }
