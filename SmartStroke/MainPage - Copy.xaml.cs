6f7abefcd8d760b9f92594c9c93ab28851e0f053
ndm4766 <melnynic@yahoo.com>
2014-02-28 20:10:41 -0600

    Create a sign-on page. Changed ordering of UI layout.

--- SmartStroke/MainPage - Copy.xaml.cs
+++ SmartStroke/MainPage - Copy.xaml.cs
@@ -20,9 +20,9 @@ namespace SmartStroke
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class MainPageCopy : Page
     {
-        public MainPage()
+        public MainPageCopy()
         {
             //2B | !2B
             //Laramie's test commit
@@ -30,17 +30,22 @@ namespace SmartStroke
             this.InitializeComponent();
         }
 
-        // Log in button clicked.
-        // Authenticate the doctor.
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void trails_click(object sender, RoutedEventArgs e)
         {
-           this.Frame.Navigate(typeof (MainPageCopy));
+            this.Frame.Navigate(typeof(TrailsMenu));
+        }
+        private void info_click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(UserInfoPage));
+        }
+        private void test_selection_click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(TestSelection));
         }
 
-        // ToDo: Create a new account
-        private void createAccount_Click(object sender, RoutedEventArgs e)
+        private void clock_click(object sender, RoutedEventArgs e)
         {
-
+            this.Frame.Navigate(typeof(ClockTest));
         }
     }
 }
