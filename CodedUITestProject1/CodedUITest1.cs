60e374e24dcf3a593ab318ce4b49be796f608e0f
subzeroice22 <laramie17goode@gmail.com>
2014-03-04 14:58:31 -0600

    added CodedUITest1 and MOCK_DATA for testing norm graphs

---
+++ CodedUITestProject1/CodedUITest1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UITesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UITest.Input;
+using Microsoft.VisualStudio.TestTools.UITest.Extension;
+using Microsoft.VisualStudio.TestTools.UITesting.HtmlControls;
+using Microsoft.VisualStudio.TestTools.UITesting.DirectUIControls;
+using Microsoft.VisualStudio.TestTools.UITesting.WindowsRuntimeControls;
+using Keyboard = Microsoft.VisualStudio.TestTools.UITesting.Keyboard;
+
+
+namespace CodedUITestProject1
+{
+    /// <summary>
+    /// Summary description for CodedUITest1
+    /// </summary>
+    [CodedUITest(CodedUITestType.WindowsStore)]
+    public class CodedUITest1
+    {
+        public CodedUITest1()
+        {
+        }
+
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", "|DataDirectory|\\MOCK_DATA.csv", "MOCK_DATA#csv", DataAccessMethod.Sequential), DeploymentItem("MOCK_DATA.csv"), TestMethod]
+
+        public void CodedUITestMethod1()
+        {
+            // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+           
+            // Launch the app
+            XamlWindow.Launch("5d90a074-2671-4811-8156-60e16c3fd5e3_ywvksadvjkdhr!App");
+            
+            // Insert username
+            this.UIMap.AssertMethod1ExpectedValues.UIUserIdEditText = TestContext.DataRow["username"].ToString();
+            //this.UIMap.AssertMethod1();
+            
+            // Insert password
+            string pass = Playback.EncryptText(TestContext.DataRow["Username"].ToString());
+            this.UIMap.UISmartStrokeWindow.UIUserPasswordEdit.Text = pass;
+
+            // Click the login button
+            Gesture.Tap(this.UIMap.UISmartStrokeWindow.UILogInButton);
+
+            // Click the add new patient button
+            Gesture.Tap(this.UIMap.UISmartStrokeWindow.UIAddnewPatientButton);
+
+            // Delete name in box
+            Gesture.Tap(this.UIMap.UISmartStrokeWindow.UIPatientNameEdit.UIDeleteButton);
+
+            // Insert patient name
+            //this.UIMap.UISmartStrokeWindow.UIPatientNameEdit = TestContext.DataRow["name"].ToString();
+            //...
+
+            // Select male
+            Gesture.Tap(this.UIMap.UISmartStrokeWindow.UIMaleRadioButton);
+
+            // Select Month
+            //...
+
+            // Select day
+            //...
+
+            // Select year
+            //...
+
+            // Select Education Level
+            //...
+
+            // Click submit button
+            Gesture.Tap(this.UIMap.UISmartStrokeWindow.UISubmitButton);
+        }
+
+        #region Additional test attributes
+
+        // You can use the following additional attributes as you write your tests:
+
+        ////Use TestInitialize to run code before running each test 
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{        
+        //    // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+        //}
+
+        ////Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{        
+        //    // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+        //}
+
+        #endregion
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+        private TestContext testContextInstance;
+
+        public UIMap UIMap
+        {
+            get
+            {
+                if ((this.map == null))
+                {
+                    this.map = new UIMap();
+                }
+
+                return this.map;
+            }
+        }
+
+        private UIMap map;
+    }
+}
