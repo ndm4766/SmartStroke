e6758ffde72a6e2668a8b15da623d152f624ed95
subzeroice22 <laramie17goode@gmail.com>
2014-03-03 22:23:08 -0600

    chart now pulls and plots patient data (except for test times). Had to comment out line 407 in TrailsTest.xaml.cs in order to compile

--- SmartStroke/NormComparison.xaml.cs
+++ SmartStroke/NormComparison.xaml.cs
@@ -28,6 +28,7 @@ namespace SmartStroke
         const string filename = "userInfo.txt";
         private NavigationHelper navigationHelper;
         private ObservableDictionary defaultViewModel = new ObservableDictionary();
+        List<PatientPlot> patientList = new List<PatientPlot>();
 
         /// <summary>
         /// This can be changed to a strongly typed view model.
@@ -63,8 +64,6 @@ namespace SmartStroke
             this.Loaded += NormComparisonPage_Loaded;
             //loadJson();
         }
-
-        
 
         /// <summary>
         /// Populates the page with content passed during navigation. Any saved state is also
@@ -126,7 +125,6 @@ namespace SmartStroke
 
         private void LoadChartContents()
         {
-
             loadJson();
 
             Random rand = new Random();
@@ -135,21 +133,46 @@ namespace SmartStroke
             List<Performance> highEducationResults = new List<Performance>();
             List<Performance> uniquePoints = new List<Performance>();
 
-            for (int i = 0; i < 100; i++)
+            for (int i = 0; i < patientList.Count; i++)
             {
-                int j = rand.Next(15,90);
-                double y = j * 0.98 + 204;
-                lowEducationResults.Add(new Performance() { Age = j, Time = rand.NextDouble() * 14 + y });
+
+                int tempAge = Convert.ToInt32(patientList[i].patientAge);
+
+                if (patientList[i].patientEducation == "Highschool Diploma")
+                {
+
+                    int j = rand.Next(tempAge - 5, tempAge + 5);
+                    double y = j * 0.82 + 200;
+                    lowEducationResults.Add(new Performance() { Age = tempAge, Time = rand.NextDouble() * 14 + y});
+
+                }
+                else
+                {
+
+                    int j = rand.Next(tempAge - 5, tempAge + 5);
+                    double y = j * 0.98 + 204;
+                    highEducationResults.Add(new Performance() { Age = tempAge, Time = rand.NextDouble() * 11 + y });
+
+                }
+
             }
 
-            for (int i = 0; i < 100; i++)
-            {
-                int j = rand.Next(15, 90);
-                double y = j * 0.82 + 200;
-                highEducationResults.Add(new Performance() { Age = j, Time = rand.NextDouble() * 11 + y });
-            }
+                /*
+                for (int i = 0; i < 100; i++)
+                {
+                    int j = rand.Next(15,90);
+                    double y = j * 0.98 + 204;
+                    lowEducationResults.Add(new Performance() { Age = j, Time = rand.NextDouble() * 14 + y });
+                }
 
-            uniquePoints.Add(new Performance() { Age = 55, Time = 260 });
+                for (int i = 0; i < 100; i++)
+                {
+                    int j = rand.Next(15, 90);
+                    double y = j * 0.82 + 200;
+                    highEducationResults.Add(new Performance() { Age = j, Time = rand.NextDouble() * 11 + y });
+                }*/
+
+                //uniquePoints.Add(new Performance() { Age = 55, Time = 260 });
 
             (ScatterChart.Series[0] as ScatterSeries).ItemsSource = lowEducationResults;
             (ScatterChart.Series[1] as ScatterSeries).ItemsSource = highEducationResults;
@@ -180,13 +203,12 @@ namespace SmartStroke
 
         async void loadJson()
         {
-            try
+            //try
             {
                 //get file
                 Windows.Storage.StorageFile myFile = await Windows.Storage.ApplicationData.Current.LocalFolder.GetFileAsync(filename);
                 //read
                 String data = await Windows.Storage.FileIO.ReadTextAsync(myFile);
-
                 patients = Windows.Data.Json.JsonArray.Parse(data);
 
                 for (uint i = 0; i < patients.Count; i++)
@@ -194,16 +216,25 @@ namespace SmartStroke
 
                     string patientName = patients.GetObjectAt(i).GetNamedValue("Name").GetString();
                     string patientBirthday = patients.GetObjectAt(i).GetNamedValue("Birthday").GetString();
-                    string patientAge = patients.GetObjectAt(i).GetNamedValue("Age").GetString();
+                    string patientBday = patients.GetObjectAt(i).GetNamedValue("Birthday").GetString();
                     string patientSex = patients.GetObjectAt(i).GetNamedValue("Sex").GetString();
                     string patientEducation = patients.GetObjectAt(i).GetNamedValue("Education").GetString();
 
+                    DateTime today = DateTime.Today;
+                    DateTime bday = Convert.ToDateTime(patientBday);
+
+                    int age = today.Year - bday.Year;
+                    if (bday > today.AddYears(-age)) age--;
+
+                    string patientAge = Convert.ToString(age);
+
                     PatientPlot patient = new PatientPlot(patientName, patientBirthday, patientAge, patientSex, patientEducation);
-                
+                    patientList.Add(patient);
+                    
                 }
 
             }
-            catch
+            //catch
             {
                 //json load failed
             }
