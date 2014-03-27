882345adb229b88fc25b110ddf8319d812b882da
imbiez <joshrispoli@gmail.com>
2014-03-27 14:59:44 -0500

    Added registration to registration page Removed  unnecessary debugging comments in TestReplay.cs and TrailsTest.xaml.cs

--- SmartStroke/CreateAccountPage.xaml.cs
+++ SmartStroke/CreateAccountPage.xaml.cs
@@ -4,8 +4,10 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -18,9 +20,12 @@ namespace SmartStroke
 {
     public sealed partial class CreateAccountPage : Page
     {
-        private bool registerUsernameReady;
-        private bool registerPasswordReady;
-        private bool registerConfirmPasswordReady;
+        private const string registeredUsersFile = "registeredUsers.txt";
+
+        private Dictionary<string, string> usernameHashDictionary;
+        //private bool registerUsernameReady;
+        //private bool registerPasswordReady;
+        //private bool registerConfirmPasswordReady;
         private string registerUsername;
         private string registerPassword;
         private string registerConfirmPassword;
@@ -39,12 +44,82 @@ namespace SmartStroke
         }
         public CreateAccountPage()
         {
+            usernameHashDictionary = new Dictionary<string, string>();
+            loadAccounts();
             this.InitializeComponent();
             this.navigationHelper = new NavigationHelper(this);
             this.navigationHelper.LoadState += navigationHelper_LoadState;
             this.navigationHelper.SaveState += navigationHelper_SaveState;
         }
-
+        
+        private string convertRegisteredUsersToString()
+        {
+            string toReturn = "";
+            foreach (KeyValuePair<string, string> K in usernameHashDictionary)
+            {
+                toReturn += K.Key;
+                toReturn += '\t';
+                toReturn += K.Value;
+                toReturn += '\n';
+            }
+            return toReturn;
+        }
+        private async Task saveToRegisteredUsersFile(string testFilename)
+        {
+            StorageFile testStorageFile;
+            string stringToSave = convertRegisteredUsersToString();
+            try
+            {
+                Task<StorageFile> fileTask = ApplicationData
+                    .Current.LocalFolder.CreateFileAsync(testFilename,
+                    CreationCollisionOption.ReplaceExisting)
+                    .AsTask<StorageFile>();
+                fileTask.Wait();
+                testStorageFile = fileTask.Result;
+                await FileIO.WriteTextAsync(testStorageFile, stringToSave);
+            }
+            catch { return; }
+        }
+        async private void saveAccounts()
+        {
+            saveToRegisteredUsersFile(registeredUsersFile);
+        }
+        async private void loadAccounts()
+        {
+            await loadRegisteredUsersFile(registeredUsersFile);
+        }
+        private async Task loadRegisteredUsersFile(string testFilename)
+        {
+            StorageFile registeredUsersStorageFile;
+            string registeredUsersString = "";
+            try
+            {
+                Task<StorageFile> fileTask = ApplicationData
+                        .Current.LocalFolder
+                        .GetFileAsync(testFilename).AsTask<StorageFile>();
+                fileTask.Wait();
+                registeredUsersStorageFile = fileTask.Result;
+                registeredUsersString 
+                    = await FileIO.ReadTextAsync(registeredUsersStorageFile);
+            }
+            catch { return; }
+            parseRegisteredUsersFile(registeredUsersString);
+        }
+        private void parseRegisteredUsersFile(string registeredUsersText)
+        {
+            List<string> testStrings =
+                registeredUsersText.Split('\n').Cast<string>()
+                .ToList<string>();
+            foreach (string line in testStrings)
+            {
+                if (line.Split('\t').Length > 1)
+                {
+                    string username = line.Split('\t')[0];
+                    string passHash = line.Split('\t')[1];
+                    usernameHashDictionary.Add(username, passHash);
+                }
+            }
+        }
         private void navigationHelper_LoadState(object sender, LoadStateEventArgs e)
         {
         }
@@ -65,6 +140,19 @@ namespace SmartStroke
         }
         private void registerButtonClicked(object sender, RoutedEventArgs e)
         {
+            if (registerPassword == null 
+                || registerUsername == null 
+                || registerConfirmPassword == null) return;
+            if (registerPassword != registerConfirmPassword) return;
+            bool usernameAlreadyExists = false;
+            foreach (KeyValuePair<string, string> K in usernameHashDictionary)
+            {
+                if (K.Key == registerUsername) usernameAlreadyExists = true;
+            }
+            if (usernameAlreadyExists) return;
+            usernameHashDictionary.Add(
+                registerUsername,registerPassword.GetHashCode().ToString());
+            saveAccounts();
             InfoPasser passer = new InfoPasser(registerUsernameInputText.Text);
             this.Frame.Navigate(typeof(PatientSelection), passer);
         }
